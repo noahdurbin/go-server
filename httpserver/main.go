ae55382e5e7ceb836f00d7272d90a9818c88931c
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-14 22:08:31 -0600

    feat: main function with root and hello paths

--- httpserver/main.go
+++ httpserver/main.go
@@ -16,3 +16,10 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
 	io.WriteString(w, "Hello, HTTP!\n")
 }
+
+func main() {
+  http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+}
