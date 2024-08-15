b0b29d1f5e3456f9b346ce22190940a4897b2842
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-14 22:12:24 -0600

    feat: error handling

--- httpserver/main.go
+++ httpserver/main.go
@@ -22,4 +22,10 @@ func main() {
 	http.HandleFunc("/hello", getHello)
 
 	err := http.ListenAndServe(":3333", nil)
+  if errors.Is(err, http.ErrServerClosed) {
+    fmt.Printf("server closed\n")
+  } else if err != nil {
+    fmt.Printf("error starting server: %s\n", err)
+    os.Exit(1)
+  }
 }
